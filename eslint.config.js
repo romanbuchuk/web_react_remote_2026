dfd598d6871546c6701877b81ce5b566af4286cd
Roman Buchuk <roman.buchuk@sigmatechnology.com>
2026-09-25 08:57:19 +0300

    add unicorn eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import unicorn from 'eslint-plugin-unicorn';
 import prettierConfig from 'eslint-config-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
@@ -13,11 +14,17 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      unicorn.configs['flat/recommended'],
       prettierConfig,
     ],
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    rules: {
+      // React components use PascalCase filenames (e.g. App.jsx), which
+      // conflicts with unicorn's default kebab-case requirement.
+      'unicorn/filename-case': 'off',
     },
   },
 ]);
